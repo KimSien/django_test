e8c4ea7ccfee14111a13bdc29c94d7c4d4f5ee9a
kim <tksok2009@gmail.com>
2018-08-16 03:15:49 +0900

    root

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
 STATIC_URL = '/static/'
 
 #import dj_database_url
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+# Heroku デプロイ時のエラーについて
+
+https://qiita.com/Kajiyu/items/446169b1a0da1e562199
+
+heroku config:set DISABLE_COLLECTSTATIC=1
+
