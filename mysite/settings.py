23417246707a9c01d9f18cb7fea23c97b3218025
kim <tksok2009@gmail.com>
2018-08-16 03:12:08 +0900

    root

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 #DATABASES['default'] = dj_database_url.config()
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #ALLOWED_HOSTS = ['*']
-#STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
